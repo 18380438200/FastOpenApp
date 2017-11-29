018aac416a7a97ebe752f1abc0b882cde9f339b5
libo <libo@goobird.com>
2017-11-29 16:16:00 +0800

    initCommit

---
+++ app/src/main/java/com/example/fastopenapp/fastopenapp/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.fastopenapp.fastopenapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+}
